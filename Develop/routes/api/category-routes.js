bd9f12b3e077fd22038354f7bf9fa246e174082c
nba251522 <inquister112345678@gmail.com>
2023-11-16 16:41:42 -0800

    added a route to get a single category instead of all

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -14,9 +14,19 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
