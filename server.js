2e675e79eba3e24eaa7f303363cfa3b4891d0d97
nba251522 <inquister112345678@gmail.com>
2023-11-21 22:24:54 -0800

    updated my pathing in my server.js file

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ require('dotenv').config();
 
 const express = require('express');
 const routes = require('./Develop/routes/api');
-const Sequelize = require('sequelize');
-const config = require('./Develop/config/config.js');
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,7 +9,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
+// Use the API routes
+app.use('/api', routes)
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
